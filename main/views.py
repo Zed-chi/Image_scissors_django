3e732390b2e4517e7ffd96b1658da5fabbf454f5
zed-chi <zed-chi@mail.ru>
2023-02-05 01:20:30 +0300

    naming fixes

--- main/views.py
+++ main/views.py
@@ -17,12 +17,14 @@ def index(request):
 
 def edit_images(request, session_id):
     """ renders Editor page for your images list """
-    dir = os.path.join(settings.MEDIA_ROOT, session_id)
+    upl_dir = os.path.join(settings.MEDIA_ROOT, session_id)
     url = f"{settings.MEDIA_URL}{session_id}"
-    image_files = os.listdir(dir)
-    images_info = [{"name": i, "url": f"{url}/{i}"} for i in image_files]    
+    image_files = os.listdir(upl_dir)
+    images_info = [
+        {"name": image, "url": f"{url}/{image}"}
+        for image in image_files
+    ]
     context = {"images": images_info, "session": session_id}
-
     return render(request, "main/editor.html", context)
 
 
@@ -30,18 +32,19 @@ def process_images(request):
     """ renders Editor page for your images list """
     if not request.method == "POST":
         return redirect("main:index")
-    if not request.POST.get("process"):
+    if "process" not in request.POST:
         return redirect("main:index")
-    s_id = request.POST.get("session")
-    print(request.POST["process"])
-    dir = os.path.join(settings.MEDIA_ROOT, s_id)
-    image_files = os.listdir(dir)
+
+    session_id = request.POST.get("session")
+    upl_dir = os.path.join(settings.MEDIA_ROOT, session_id)
+    image_filenames = os.listdir(upl_dir)
     values = json.loads(request.POST["process"])
-    for file in image_files:
-        attrs = values[file]
-        filepath = os.path.join(dir, file)
-        im = Image.open(filepath)
-        width, height = im.size
+
+    for filename in image_filenames:
+        attrs = values[filename]
+        filepath = os.path.join(upl_dir, filename)
+        image = Image.open(filepath)
+        width, height = image.size
         rows = int(attrs["row"])
         cols = int(attrs["col"])
         img_width = round((width / 100) * (int(attrs["width"]) / cols))
@@ -50,11 +53,11 @@ def process_images(request):
         top_offset = round((height / 100) * (int(attrs["top"])))
 
         # Создание изображений
-        result_dir = f"IMAGES_RESULT_DIR/{s_id}"
+        result_dir = f"{settings.IMAGES_RESULT_DIR}/{session_id}"
         os.makedirs(result_dir, exist_ok=True)
         for row in range(rows):
             for col in range(cols):
-                region = im.crop(
+                region = image.crop(
                     [
                         left_offset,
                         top_offset,
@@ -62,7 +65,7 @@ def process_images(request):
                         top_offset + img_height,
                     ]
                 )
-                name, ext = file.split(".")
+                name, ext = filename.split(".")
                 tile_path = (
                     f"{result_dir}/{name}-{left_offset}-{top_offset}.{ext}"
                 )
@@ -70,14 +73,14 @@ def process_images(request):
                 left_offset += img_width
             top_offset += img_height
     result_images = os.listdir(result_dir)
-    with ZipFile(f"{settings.IMAGES_RESULT_DIR}{s_id}.zip", "w") as arc:
+    with ZipFile(f"{settings.IMAGES_RESULT_DIR}{session_id}.zip", "w") as arc:
         for path in result_images:
             arc.write(f"{result_dir}/{path}")
     try:
-        with open(f"{s_id}.zip", "rb") as arc:
+        with open(f"{session_id}.zip", "rb") as arc:
             file_data = arc.read()
         response = HttpResponse(file_data, content_type="application/zip")
-        response["Content-Disposition"] = f'attachment; filename="{s_id}.zip"'
+        response["Content-Disposition"] = f'attachment; filename="{session_id}.zip"'
     except IOError:
         response = HttpResponseNotFound("<h1>File not exist</h1>")
     return response
