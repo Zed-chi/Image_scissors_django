1d70a1f76f88442bd68383524c58ace1230b51a7
zed-chi <zed-chi@mail.ru>
2023-02-18 17:37:41 +0000

    relative path fix

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from datetime import datetime
+from os.path import isdir, isfile
 
 from django.apps import AppConfig
 from django.conf import settings
@@ -10,17 +11,24 @@ from django.core.signals import request_started
 def my_callback(sender, **kwargs):
     now = datetime.now()
     up = [
-        f"{settings.IMAGES_UPLOAD_DIR}/{i}"
-        for i in os.listdir(settings.IMAGES_UPLOAD_DIR)
+        f"{settings.BASE_DIR}/{settings.IMAGES_UPLOAD_DIR}/{i}"
+        for i in os.listdir(
+            os.path.join(settings.BASE_DIR, settings.IMAGES_UPLOAD_DIR)
+        )
     ]
     res = [
-        f"{settings.IMAGES_RESULT_DIR}/{i}"
-        for i in os.listdir(settings.IMAGES_RESULT_DIR)
+        f"{settings.BASE_DIR}/{settings.IMAGES_RESULT_DIR}/{i}"
+        for i in os.listdir(
+            os.path.join(settings.BASE_DIR, settings.IMAGES_RESULT_DIR)
+        )
     ]
-    for file in up + res:
-        created_at = datetime.fromtimestamp(os.stat(file).st_ctime)
+    for elem in up + res:
+        created_at = datetime.fromtimestamp(os.stat(elem).st_ctime)
         if (now - created_at).seconds > settings.IMG_LIFE_SEC:
-            shutil.rmtree(file)
+            if isdir(elem):
+                shutil.rmtree(elem)
+            if isfile(elem):
+                os.remove(elem)
 
 
 class MainConfig(AppConfig):
