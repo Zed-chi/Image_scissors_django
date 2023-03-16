2915948dd63767317188516632036dc0c8a35d2a
zed-chi <zed-chi@mail.ru>
2023-03-16 16:24:14 +0300

    added init dirs creation step

--- manage.py
+++ manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
+
+
+def make_dirs():
+    upl_path = os.path.join(os.getcwd(), settings.IMAGES_UPLOAD_DIR)
+    res_path = os.path.join(os.getcwd(), settings.IMAGES_RESULT_DIR)
+    os.makedirs(upl_path, exist_ok=True)
+    os.makedirs(res_path, exist_ok=True)
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_scissors.settings")
+    make_dirs()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +29,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
